0b263c8f2211809da10a50d8b88137b7f27f0f4b
Rupendra-coder <57608083+Rupendra-coder@users.noreply.github.com>
2022-01-28 23:27:11 +0545

    Adding User Model

---
+++ models/UserModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+//const mongoose = require("mongoose");
+import bcrypt from "bcryptjs";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Login
+
+const User = mongoose.model("User", userSchema);
+
+
+export default User;
