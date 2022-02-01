1c3b3d2fb608c68e8a6aa18a3d5a313edeec6520
Rupendra-coder <57608083+Rupendra-coder@users.noreply.github.com>
2022-02-01 09:30:13 +0545

    Editing order model

--- models/OrderModel.js
+++ models/OrderModel.js
@@ -8,19 +8,7 @@ const orderSchema = mongoose.Schema(
       required: true,
       ref: "User",
     },
-    orderItems: [
-      {
-        name: { type: String, required: true },
-        qty: { type: Number, required: true },
-        image: { type: String, required: true },
-        price: { type: Number, required: true },
-        product: {
-          type: mongoose.Schema.Types.ObjectId,
-          required: true,
-          ref: "Product",
-        },
-      },
-    ],
+    
     shippingAddress: {
       address: { type: String, required: true },
       city: { type: String, required: true },
@@ -75,6 +63,7 @@ const orderSchema = mongoose.Schema(
   }
 );
 
+const Order = mongoose.model("Order", orderSchema);
 
 export default Order;
 
